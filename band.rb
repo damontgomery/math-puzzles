95a73f30b37c9d3259e7a54e95d6228d192ad164
Daniel Montgomery <damontgomery@gmail.com>
2020-08-07 00:11:25 -0500

    Use 'find_all' since it shows up in the code completion.

--- band.rb
+++ band.rb
@@ -8,7 +8,7 @@ while factors.length != target do
   # Pick the next number.
   number += 1
 
-  factors = (1..number).to_a.filter do |potential_factor|
+  factors = (1..number).find_all do |potential_factor|
     (number % potential_factor).zero?
   end
 end
