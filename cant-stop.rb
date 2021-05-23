a52a8f96100a1fb4fb19e1bda34fb28c988c066f
Daniel Montgomery <damontgomery@gmail.com>
2021-05-23 17:29:52 -0500

    use permutations and combinations

--- cant-stop.rb
+++ cant-stop.rb
@@ -1,52 +1,35 @@
 die = 1..6
 minSpace = 2
 maxSpace = 12
-# spaces = minSpace..maxSpace
+spaces = minSpace..maxSpace
 # The combinations are less than 1 million 6^4 * 12c3, so let's just find exact values rather than simulate.
 
 results = {}
 
-# Todo: make this a function?
-# Pick spaces
-(minSpace..maxSpace).each do |space1|
-  ((space1 + 1)..maxSpace).each do |space2|
-    ((space2 + 1)..maxSpace).each do |space3|
-      spaceKey = "#{space1}:#{space2}:#{space3}"
-      results[spaceKey] = 0
+spaces.to_a.combination(3).to_a.each do |selectedSpaces|
+  spaceKey = "#{selectedSpaces[0]}:#{selectedSpaces[1]}:#{selectedSpaces[2]}"
 
-      # Roll dice
-      die.each do |die1|
-        die.each do |die2|
-          die.each do |die3|
-            die.each do |die4|
-              spaces = [space1, space2, space3]
-              dice = [die1, die2, die3, die4]
-              
-              # Calculate combinations
-              combinations = []
+  results[spaceKey] = 0
 
-              dice.each_with_index do |dieA, dieAIndex|
-                dice.slice((dieAIndex + 1)..-1).each do |dieB|
-                  combinations.push(dieA + dieB)
-                end
-              end
+  die.to_a.repeated_permutation(4).to_a.each do |dice| 
+    # Calculate combinations
+    combinations = []
 
-              combinations.uniq!
-
-              # Check if combinations found a match.
-              match = false
-
-              spaces.each do |space|
-                match = true if combinations.include?(space)
-              end
-
-              # Record results
-              results[spaceKey] += 1 if match
-            end
-          end
-        end
-      end
+    dice.combination(2).to_a.each do |combinedDice|
+      combinations.push(combinedDice[0] + combinedDice[1])
     end
+
+    combinations.uniq!
+
+    # Check if combinations found a match.
+    match = false
+
+    selectedSpaces.each do |space|
+      match = true if combinations.include?(space)
+    end
+
+    # Record results
+    results[spaceKey] += 1 if match
   end
 end
 
